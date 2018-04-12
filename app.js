a8c1c7cba90988604cb5f2263b3699ae300ce223
JoeyM <joeytermeulen@gmail.com>
2018-04-12 15:54:23 +0200

    Added english pages

--- app.js
+++ app.js
@@ -8,22 +8,42 @@ app.get("/", function (req, res) {
     res.redirect("/home");
 });
 
+// DUTCH ROUTES
 app.get("/home", function (req, res) {
-    res.render("home");
+    res.render("dutch/home");
 });
 
 app.get("/over-ons", function (req, res) {
-    res.render("over-ons");
+    res.render("dutch/over-ons");
 });
 
 app.get("/vakwerk", function (req, res) {
-    res.render("vakwerk");
+    res.render("dutch/vakwerk");
 });
 
 app.get("/contact", function (req, res) {
-    res.render("contact");
+    res.render("dutch/contact");
 });
 
+// ENGLISH ROUTES
+app.get("/home-en", function (req, res) {
+    res.render("english/home");
+});
+
+app.get("/about-us-en", function (req, res) {
+    res.render("english/over-ons");
+});
+
+app.get("/craftmanship-en", function (req, res) {
+    res.render("english/vakwerk");
+});
+
+app.get("/contact-en", function (req, res) {
+    res.render("english/contact");
+});
+
+
+// CATCH 404 ERROR
 app.use(function (req, res, next) {
     res.status(404).render("error");
 });
