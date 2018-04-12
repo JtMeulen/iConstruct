7257646edf61c0f459407d3ab5506a94f468af1a
JoeyM <joeytermeulen@gmail.com>
2018-04-12 14:42:34 +0200

    Added more pages and styling

--- app.js
+++ app.js
@@ -4,18 +4,30 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.redirect("/home");
 });
 
-app.get("/home", function(req, res){
+app.get("/home", function (req, res) {
     res.render("home");
 });
 
-app.get("/contact", function(req, res){
+app.get("/over-ons", function (req, res) {
+    res.render("over-ons");
+});
+
+app.get("/vakwerk", function (req, res) {
+    res.render("vakwerk");
+});
+
+app.get("/contact", function (req, res) {
     res.render("contact");
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+app.use(function (req, res, next) {
+    res.status(404).render("error");
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
     console.log("SERVER RUNNING", process.env.PORT);
 });
